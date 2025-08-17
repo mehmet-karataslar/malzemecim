006a4321730696880e92ddc5c625e921ac2b353d
Mehmet <mehmetkarataslar@gmail.com>
2025-08-17 09:57:24 +0300

    Fix Android build configuration - NDK 27.0.12077973, minSdk 23, Java 17 upgrade

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,16 +10,16 @@ plugins {
 
 android {
     namespace = "com.example.malzemecim"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 35
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     defaultConfig {
@@ -27,8 +27,8 @@ android {
         applicationId = "com.example.malzemecim"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        minSdk = 23
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
